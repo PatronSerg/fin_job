c92b34e6d85e59a87f0053e7fabed9aaf0aedeb7
Serg <patron.serg@yandex.ru>
2023-09-26 13:14:06 +0300

    filter_array

--- one_try/Program.cs
+++ one_try/Program.cs
@@ -1,4 +1,24 @@
-﻿
-        Console.Write("Введите элементы массива через пробел: ");
-        string[] input = Console.ReadLine().Split(' ');
+﻿static string[] FilterArray(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (ElementValid(array[i]))
+        {
+            count++;
+        }
+    }
 
+    string[] result = new string[count];
+    int index = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (ElementValid(array[i]))
+        {
+            result[index] = array[i];
+            index++;
+        }
+    }
+
+    return result;
+}
