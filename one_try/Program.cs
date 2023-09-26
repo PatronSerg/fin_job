6cd6bd42e7c6a3e4a973396121e482e5ea094214
Serg <patron.serg@yandex.ru>
2023-09-26 14:05:34 +0300

    start_main

--- one_try/Program.cs
+++ one_try/Program.cs
@@ -1,4 +1,11 @@
-﻿
-        Console.Write("Введите элементы массива через пробел: ");
-        string[] input = Console.ReadLine().Split(' ');
+﻿static void Main()
+{
+    Console.Write("Введите элементы массива через пробел: ");
+    string input = Console.ReadLine();
+    string[] array = input.Split(' ');
+    string[] result = FilterArray(array);
+
+    Console.WriteLine("Новый массив:");
+    PrintArray(result);
+}
 
