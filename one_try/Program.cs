477c0ee5e32e3591bd63cda059529525971c5aac
Serg <patron.serg@yandex.ru>
2023-09-26 14:42:42 +0300

    merge

--- one_try/Program.cs
+++ one_try/Program.cs
@@ -1,13 +1,11 @@
-static void Main()
-{
-    Console.Write("Введите элементы массива через пробел: ");
-    string input = Console.ReadLine();
-    string[] array = input.Split(' ');
-    string[] result = FilterArray(array);
 
-    Console.WriteLine("Новый массив:");
-    PrintArray(result);
-}
+Console.Write("Введите элементы массива через пробел: ");
+string input = Console.ReadLine();
+string[] array = input.Split(' ');
+string[] result = FilterArray(array);
+
+Console.WriteLine("Новый массив:");
+PrintArray(result);
 
 
 static string[] FilterArray(string[] array)
